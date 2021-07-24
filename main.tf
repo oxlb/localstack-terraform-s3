b7ab249685ef3182a7a35ccee312a65526375b83
Onexlab <46291202+oxlb@users.noreply.github.com>
2021-07-24 12:12:53 +0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test123"
+  secret_key                  = "testabc"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
+
+# Create Bucket
+resource "aws_s3_bucket" "b" {
+  bucket = "onexlab-bucket-terraform"
+  acl    = "public-read"
+}
+
+# Upload an object
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.b.id
+  key    = "test.txt"
+  acl    = "public-read"
+  source = "test.txt"
+}
